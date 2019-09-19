61f0f259d50ca34cebd73e3091e191814547bf7e
andri <andri.yuman@cmn.co.id>
2019-09-19 16:18:59 +0700

    delete

--- src/test/java/com/dana/bijak/BookRepositoryTest.java
+++ src/test/java/com/dana/bijak/BookRepositoryTest.java
@@ -12,26 +12,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
 public class BookRepositoryTest {
-
-    @Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
-    private BookRepository repository;
-
-    @Test
-    public void testFindByName() {
-
-        entityManager.persist(new Book("C++"));
-
-        List<Book> books = repository.findByName("C++");
-        assertEquals(1, books.size());
-
-        assertThat(books).extracting(Book::getName).containsOnly("C++");
-
-    }
 
 }
